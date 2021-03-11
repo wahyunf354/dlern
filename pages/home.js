b70c3951e9128bcd243a7fc8a6f2fdab019a6059
wahyunf354 <wahyunurfadillah313@gmail.com>
2021-03-11 17:38:34 +0700

    keren; ;

--- pages/home.js
+++ pages/home.js
@@ -3,18 +3,31 @@ import firebase from "../config/firebase";
 import useRouter from "next/router";
 import Header from "../components/molekul/Header";
 import Button from "../components/atom/Button";
+import Image from "next/image";
 
 function ContentHome(props) {
   return (
     <>
-      <div className="container mx-auto grid md:grid-cols-2 grid-cols-1 gap-4 pt-10">
+      <div className="container md:px-36 px-5 mx-auto grid md:grid-cols-2 grid-cols-1 gap-10 pt-10">
         <div className="md:col-span-2">
           <h1 className="text-blue-900 text-2xl font-bold">
             Hi, {props.nameUser}
           </h1>
         </div>
-        <Button className="py-4" hasShadow hasRounded>
-          Kosa Kata
+        <Button
+          className="py-4 border-1"
+          href=""
+          type="link"
+          hasShadow
+          hasRounded
+        >
+          <Image
+            src="/assets/illustrasi/kosa-kata.png"
+            width={200}
+            height={140}
+            layout="responsive"
+          />
+          <h2>Kosa Kata</h2>
         </Button>
         <Button className="py-4" hasShadow hasRounded>
           Games
