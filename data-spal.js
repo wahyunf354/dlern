71a109d16b122415548c6faa2a1aa6cc0581ca6e
wahyunf354 <wahyunurfadillah313@gmail.com>
2021-03-18 16:12:21 +0700

    membuat dummy soal

---
+++ data-spal.js
@@ -0,0 +1,22 @@
+export default {
+  data: [
+    {
+      level: 1,
+      soals: [
+        {
+          type: "s11",
+          soal: {
+            pertanyaan: "Gambar mana yang orang?",
+            pilihan: [
+              "https://placeimg.com/512/512/animals",
+              "https://placeimg.com/512/512/arch",
+              "https://placeimg.com/512/512/nature",
+              "https://placeimg.com/512/512/people",
+            ],
+            jawaban: "https://placeimg.com/512/512/people",
+          },
+        },
+      ],
+    },
+  ],
+};
