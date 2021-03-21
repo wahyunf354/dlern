f3f3805a7beb99d4ba309aad363e675b99b60773
wahyunf354 <wahyunurfadillah313@gmail.com>
2021-03-21 12:21:56 +0700

    sudah banyak sampe lupa

--- pages/kosa-kata/preposisi.js
+++ pages/kosa-kata/preposisi.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from "react";
 import Layout from "../../components/Layout";
-
+import Header from "../../components/molekul/Header";
+import ItemKata from "../../components/molekul/ItemKata";
 
 function Proposisi() {
   return (
     <Layout title="Proposisi">
-      Proposisi
+      <Header isBack href="/kosa-kata" />
+      <main className="container mt-5 mx-auto px-5 md:px-24 grid grid-cols-2 md:grid-cols-6 gap-4">
+        <h1 className="col-span-2 md:col-span-6 text-4xl font-bold text-gray-900">
+          Präpositionen
+        </h1>
+      </main>
     </Layout>
-  )
+  );
 }
 
-export default Proposisi
+export default Proposisi;
