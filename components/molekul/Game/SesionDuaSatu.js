9b61dc730a296c5ca559d7cb59a8988ceedea0cc
wahyunf354 <wahyunurfadillah313@gmail.com>
2021-03-24 20:25:14 +0700

    Selesai membuat tampilan s22 s21

---
+++ components/molekul/Game/SesionDuaSatu.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import InputText from "../../atom/InputText";
+import propTypes from "prop-types";
+import Button from "../../atom/Button";
+import Fade from "react-reveal";
+
+function SesionSatuTiga({ soal, handleClickAnswer }) {
+  const [answer, setAnswer] = useState([]);
+
+  const handleClick = (answer) => {
+    if (answer == soal.jawaban) {
+      handleClickAnswer(true);
+    } else {
+      handleClickAnswer(false);
+    }
+  };
+
+  const handleClickChoice = (choice) => {
+    setAnswer([...answer, choice]);
+  };
+
+  const handleRemoveChoice = (choice) => {
+    const tmpArr = answer;
+    const indexWillRemove = tmpArr.indexOf(choice);
+    if (indexWillRemove !== -1) {
+      tmpArr.splice(indexWillRemove, 1);
+      setAnswer([...tmpArr]);
+    }
+  };
+
+  return (
+    <Fade>
+      <main className="container flex items-center flex-col mx-auto p-5 gap-3 relative">
+        <h1 className="text-xl">Tulis dalam Bahasa Indonesia</h1>
+        <p className="mb-10 text-gray-500 text-xl">{soal.pertanyaan}</p>
+
+        <div className="text-center p-3 mb-10 border h-28 md:w-1/2 w-full rounded-lg">
+          {answer.map((answer, i) => (
+            <Button
+              isSecondary
+              hasRounded
+              hasShadow
+              className="mb-2 mx-1"
+              key={i}
+              onClick={() => handleRemoveChoice(answer)}
+            >
+              <p>{answer}</p>
+            </Button>
+          ))}
+        </div>
+
+        <div className="text-center">
+          {soal.pilihan.map((choice, i) => {
+            return (
+              answer.every((current) => current !== choice) && (
+                <Button
+                  isSecondary
+                  hasRounded
+                  hasShadow
+                  className="m-1"
+                  key={i}
+                  onClick={() => handleClickChoice(choice)}
+                >
+                  <p>{choice}</p>
+                </Button>
+              )
+            );
+          })}
+        </div>
+
+        <div className="w-full md:w-1/2 mt-16">
+          <Button
+            isPrimary
+            hasShadow
+            hasRounded
+            className="w-full"
+            onClick={() => handleClick(answer.join(" "))}
+          >
+            Lanjut
+          </Button>
+        </div>
+      </main>
+    </Fade>
+  );
+}
+
+SesionSatuTiga.propTypes = {
+  soal: propTypes.object.isRequired,
+  handleClickAnswer: propTypes.func,
+};
+
+export default SesionSatuTiga;
--- pages/games/soal.js
+++ pages/games/soal.js
@@ -7,24 +7,78 @@ import PopupWrong from "../../components/molekul/Game/PopupWrong";
 import SesionSatuSatu from "../../components/molekul/Game/SesionSatuSatu";
 import Button from "../../components/atom/Button";
 import SesionSatuTiga from "../../components/molekul/Game/SesionSatuTiga";
-
+import SesionDuaSatu from "../components/molekul/Game/SesionDuaSatu";
 import SesionSatuDua from "../../components/molekul/Game/SesionSatuDua";
 import firebase from "../../config/firebase";
 import Spinner from "../../components/atom/Spinner";
 
-const FinistGame = ({ coin }) => {
+const FinistGame = ({ coin, reset, home }) => {
   return (
     <div className="h-screen flex justify-center items-center">
-      <div 	
-				className="w-60 h-36 bg-yellow rounded-lg shadow-lg flex justify-center items-center"
-		>
-				<h1 className="text-white font-bold text-4xl"> {coin} </h1>
-				<Button>
-					Lanjut
-				</Button>
-				<Button>Home</Button>
-				<Button>Ulang</Button>
-			</div>
+      <div className="w-96 h-60 bg-yellow rounded-lg shadow-lg flex flex-col justify-center items-center">
+        <p className="text-white text-xl font-light">Koin yang kamu dapat</p>
+        <h1 className="text-white font-bold text-4xl mb-4"> {coin} </h1>
+        <div className="grid gap-4 grid-cols-3">
+          <Button
+            onClick={() => reset()}
+            className="bg-white p-2"
+            hasRounded
+            hasShadow
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="text-yellow w-8 h-8 font-bold"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M3 10h10a8 8 0 018 8v2M3 10l6 6m-6-6l6-6"
+              />
+            </svg>
+          </Button>
+          <Button
+            onClick={() => home()}
+            className="bg-white p-2"
+            hasRounded
+            hasShadow
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="text-yellow w-8 h-8 "
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+              />
+            </svg>
+          </Button>
+          <Button className="bg-white p-2" hasShadow hasRounded>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="text-yellow w-8 h-8"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 5l7 7-7 7"
+              />
+            </svg>
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
@@ -38,6 +92,7 @@ const Soal = () => {
   const [isWrong, setIsWrong] = useState(false);
   const [isShowFinist, setIsShowFinist] = useState(false);
   const level = useRouter().query.level;
+  const router = useRouter();
 
   useEffect(() => {
     if (level)
@@ -77,10 +132,53 @@ const Soal = () => {
     }
   };
 
+  const resetCoin = () => {
+    console.log("hello");
+    setCoin(0);
+    setCurrentQuestion(0);
+    setIsShowFinist(false);
+    setIsCorrect(false);
+    setIsWrong(false);
+  };
+
+  const updateToFirebase = (koin, level) => {
+    return new Promise((resolve, reject) => {
+      const user = firebase.auth().currentUser;
+      if (user) {
+        console.log(user.uid);
+        const uid = user.uid;
+        firebase
+          .firestore()
+          .collection("users")
+          .doc(uid)
+          .update({
+            koin,
+            level,
+          })
+          .then((result) => {
+            resolve(result);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      }
+    });
+  };
+
+  const handleToGameHome = () => {
+    //    updateToFirebase(coin, level)
+    //      .then(() => {
+    //        console.log("Success Add Data");
+    //        router.push("/games");
+    //      })
+    //      .catch((err) => console.log("Error: ", err));
+    console.log("Seharusnya ke home games");
+  };
+
   return (
     <Layout className="h-screen" title={`Games | Level ${level ? level : ""}`}>
       {isShowFinist ? (
-        <FinistGame coin={coin} />
+        <FinistGame coin={coin} reset={resetCoin} home={handleToGameHome} />
       ) : (
         <>
           <Header isBack href="/games" />
@@ -113,6 +211,13 @@ const Soal = () => {
               {currentSoals[currectQuestion].type == "s13" && (
                 <SesionSatuTiga
                   soal={currentSoals[currectQuestion].soal}
+                  handleClickAnswer={handleClickAnswer}
+                />
+              )}
+
+              {currentSoals[currectQuestion].type == "s21" && (
+                <SesionDuaSatu
+                  soal={currentSoals[currectQuestion.soal]}
                   handleClickAnswer={handleClickAnswer}
                 />
               )}
