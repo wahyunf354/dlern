8cacacabf1519a8e4c436e1ebd4ffb8d80e7918a
wahyunf354 <wahyunurfadillah313@gmail.com>
2021-04-03 11:28:05 +0700

    buat tampilan buttom change profil

--- pages/profil.js
+++ pages/profil.js
@@ -26,6 +26,10 @@ const Profil = () => {
       });
   };
 
+  const hendleUploadProfile = () => {
+    console.log("upload");
+  };
+
   if (!user.name) {
     const currentUser = firebase.auth().currentUser;
     console.log(currentUser);
@@ -56,7 +60,46 @@ const Profil = () => {
         </div>
       ) : (
         <div className="container pt-8 mx-auto flex flex-col items-center">
-          <div className="mb-5 bg-gray-400 w-40 h-40 rounded-full"></div>
+          <div className="mb-5 bg-gray-300 w-40 h-40 rounded-full relative">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="text-gray-100 w-40 h-40"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <div className="w-10 h-10 bg-green-900  rounded-full absolute bottom-0 right-0 flex justify-center items-center">
+              <Button onClick={hendleUploadProfile}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="w-7 h-7 text-white"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+              </Button>
+            </div>
+          </div>
           <h3 className="text-5xl mb-5 text-green-900 font-font">
             {user.name}
           </h3>
